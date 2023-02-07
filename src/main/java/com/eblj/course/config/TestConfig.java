1cb1d8a780fb936c048f2b09ac1f3e770952b2fc
junior brandao <edilson_brandaojunior@hotmail.com>
2023-02-07 20:17:56 -0300

    Payment, one-to-one association

--- src/main/java/com/eblj/course/config/TestConfig.java
+++ src/main/java/com/eblj/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.eblj.course.entities.Category;
 import com.eblj.course.entities.Order;
 import com.eblj.course.entities.OrderItem;
+import com.eblj.course.entities.Payment;
 import com.eblj.course.entities.Product;
 import com.eblj.course.entities.User;
 import com.eblj.course.entities.enuns.OrderStatus;
@@ -83,6 +84,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orederRepository.save(o1);
 	} 
 
 }
