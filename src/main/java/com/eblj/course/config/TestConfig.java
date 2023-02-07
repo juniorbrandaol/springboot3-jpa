d461b3dd860147fc837c2f954241a3dc8ef14b5b
junior brandao <edilson_brandaojunior@hotmail.com>
2023-02-07 17:21:53 -0300

    Product

--- src/main/java/com/eblj/course/config/TestConfig.java
+++ src/main/java/com/eblj/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.eblj.course.entities.Category;
 import com.eblj.course.entities.Order;
+import com.eblj.course.entities.Product;
 import com.eblj.course.entities.User;
 import com.eblj.course.entities.enuns.OrderStatus;
 import com.eblj.course.repositories.CategoryRepository;
 import com.eblj.course.repositories.OrderRepository;
+import com.eblj.course.repositories.ProductRepository;
 import com.eblj.course.repositories.UserRepository;
 
 @Configuration
@@ -28,6 +30,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private CategoryRepository categoryRepository; 
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 
 	@Override
@@ -45,7 +50,14 @@ public class TestConfig implements CommandLineRunner {
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
 		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+		
 		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
 		userRepository.saveAll(Arrays.asList(u1,u2,u3));
 		orederRepository.saveAll(Arrays.asList(o1,o2,o3));
---
+++ src/main/java/com/eblj/course/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.eblj.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eblj.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	
+}
