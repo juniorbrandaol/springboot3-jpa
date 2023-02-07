8d2598264b7f8d2000e6f158ee2123886027019d
junior brandao <edilson_brandaojunior@hotmail.com>
2023-02-07 16:19:13 -0300

    Category

--- src/main/java/com/eblj/course/config/TestConfig.java
+++ src/main/java/com/eblj/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.eblj.course.entities.Category;
 import com.eblj.course.entities.Order;
 import com.eblj.course.entities.User;
 import com.eblj.course.entities.enuns.OrderStatus;
+import com.eblj.course.repositories.CategoryRepository;
 import com.eblj.course.repositories.OrderRepository;
 import com.eblj.course.repositories.UserRepository;
 
@@ -23,6 +25,10 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orederRepository; 
+	
+	@Autowired
+	private CategoryRepository categoryRepository; 
+
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -34,6 +40,12 @@ public class TestConfig implements CommandLineRunner {
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAINTING, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAINTING, u1);
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
 		
 		userRepository.saveAll(Arrays.asList(u1,u2,u3));
 		orederRepository.saveAll(Arrays.asList(o1,o2,o3));
