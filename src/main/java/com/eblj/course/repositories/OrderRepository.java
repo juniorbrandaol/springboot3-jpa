7475c702146813a73eee52dc18194ffed1efa8be
junior brandao <edilson_brandaojunior@hotmail.com>
2023-02-07 14:01:51 -0300

    Order, Instant, ISO 8601 PART 2

---
+++ src/main/java/com/eblj/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.eblj.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eblj.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+	
+}
