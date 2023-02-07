a815578689da56b63b9e262a9c4fa860a327edb1
junior brandao <edilson_brandaojunior@hotmail.com>
2023-02-07 11:47:48 -0300

    Service layer, component registration

--- src/main/java/com/eblj/course/config/TestConfig.java
+++ src/main/java/com/eblj/course/config/TestConfig.java
@@ -20,10 +20,12 @@ public class TestConfig implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		User u1 = new User(null, "Edilson Bfandão", "edilson@gmail.com", "988888888", "123456");
+		User u2 = new User(null, "Keylla Brandão", "keylla@gmail.com", "977777777", "123456");
+		User u3 = new User(null, "Gabriel Brandão", "gabriel@gmail.com", "977777777", "123456");
+		User u4 = new User(null, "Rute Brandão", "rute@gmail.com", "977777777", "123456");
 		
-		userRepository.saveAll(Arrays.asList(u1,u2));
+		userRepository.saveAll(Arrays.asList(u1,u2,u3,u4));
 		
 	} 
 
