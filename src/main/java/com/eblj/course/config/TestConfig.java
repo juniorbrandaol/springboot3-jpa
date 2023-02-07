f4baf364bf9a3f9c3c4c39cb9837d92e282919fe
junior brandao <edilson_brandaojunior@hotmail.com>
2023-02-07 19:29:18 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/eblj/course/config/TestConfig.java
+++ src/main/java/com/eblj/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.eblj.course.entities.Category;
 import com.eblj.course.entities.Order;
+import com.eblj.course.entities.OrderItem;
 import com.eblj.course.entities.Product;
 import com.eblj.course.entities.User;
 import com.eblj.course.entities.enuns.OrderStatus;
 import com.eblj.course.repositories.CategoryRepository;
+import com.eblj.course.repositories.OrderItemRepository;
 import com.eblj.course.repositories.OrderRepository;
 import com.eblj.course.repositories.ProductRepository;
 import com.eblj.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 
 	@Override
@@ -70,6 +75,13 @@ public class TestConfig implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1,u2,u3));
 		orederRepository.saveAll(Arrays.asList(o1,o2,o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
 	} 
 
