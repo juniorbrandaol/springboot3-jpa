78677ad6195f00089653b79f3b6cbd75555b8936
junior brandao <edilson_brandaojunior@hotmail.com>
2023-02-08 11:23:34 -0300

    Exception handling - findById

---
+++ src/main/java/com/eblj/course/services/exception/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.eblj.course.services.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found.Id: " +id);
+	}
+
+}
